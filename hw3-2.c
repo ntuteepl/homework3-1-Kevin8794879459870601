7cc759b5ab103bd400051f56aa5a0613591aee29
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-19 19:54:57 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
-main()
-{
-int i, j, k,l,m,n,o;
-scanf("%d%d%d%d%d%d", &i, &j, &k,&l,&m,&n);
 
-if (k<j){
-    o++;
-}
-if(m<l){
-    o++;
-}
-if(m<j){
-    o++;
-}
-if (k<j&&m>l){o--;}
-printf("%d\n", o);
-return 0;
+typedef struct {
+    int start;
+    int end;
+} Mission;
+
+int main() {
+    int a,b,c,d,e,f;
+    scanf("%d%d%d%d%d%d", &a, &b, &c,&d,&e,&f);
+    Mission missions[3] = {
+        {a, b},
+        {c, d},
+        {e, f}
+    };
+
+    int num_vehicles = 0;
+    int end_times[3] = {0, 0, 0};
+
+
+    for (int i = 0; i < 3; i++) {
+        int assigned = 0;
+
+
+        for (int j = 0; j < num_vehicles; j++) {
+            if (missions[i].start >= end_times[j]) {
+                end_times[j] = missions[i].end;
+                assigned = 1;
+                break;
+            }
+        }
+
+        if (!assigned) {
+            end_times[num_vehicles] = missions[i].end;
+            num_vehicles++;
+        }
+    }
+
+    printf("%d\n", num_vehicles);
+
+    return 0;
 }
