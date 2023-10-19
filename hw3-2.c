03e38f96955e5d955c5cfde2d64c4c5906991f68
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-19 19:27:43 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+main()
+{
+int i, j, k,l,m,n,o;
+scanf("%d%d%d%d%d%d", &i, &j, &k,&l,&m,&n);
+
+if (k<j){
+    o++;
+}
+if(m<l){
+    o++;
+}
+if(m<j){
+    o++;
+}
+printf("%d\n", o);
+return 0;
+}
 
