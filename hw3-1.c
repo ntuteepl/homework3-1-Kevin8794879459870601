ddc1bfe279bb9f511039422ac3150e412f2ddc14
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-19 19:27:09 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+main()
+{
+int i,j;
+scanf("%d", &i);
+    j=i-1;
+	while(j>2){
+            if (i%j==0){
+                printf("NO");
+                break;
+            }
+
+            j=j-1;
+
+	}
+	if (i%j>0||i==2){
+                printf("YES");
+
+            }
+
+}
 
